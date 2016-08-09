8ddf14a34f85021a70550dcb6ba39fb45783cf26
Julio Batista Silva <julio@juliobs.com>
2016-08-09 16:32:11 -0300

    Erro de conexao

--- interface/conexao.php
+++ interface/conexao.php
@@ -5,7 +5,8 @@ $db_pass = "labbd123";
 $db_host = "127.0.0.1";
 $con = mysqli_connect($db_host,$db_user,$db_pass,$db_name);
 
-if(!$con){
-    die("Não foi possível conectar ao banco de dados");
+// Verifica conexao
+if ($conn->connect_error) {
+    die("Falha na conexão: " . $conn->connect_error);
 }
 ?>
