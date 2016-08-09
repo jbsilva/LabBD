0a0ebab332c9b694ee5c95fb2c8ae0f58f0c4b7f
Julio Batista Silva <julio@juliobs.com>
2016-08-09 16:29:46 -0300

    Troca tab por espaços

--- interface/pessoa.php
+++ interface/pessoa.php
@@ -8,11 +8,11 @@ $sql_query = "SELECT * FROM siga.tbl_pessoa where pessoa_id = $CPF";
 $result = mysqli_query($con, $sql_query);
 
 if(mysqli_num_rows($result)>0) {
-	$row = mysqli_fetch_assoc($result);
+    $row = mysqli_fetch_assoc($result);
     $prenome = $row["prenome"];
     $sobrenome = $row["sobrenome"];
     echo "Nome: ".$prenome." ".$sobrenome;
 } else {
-	echo "Pessoa não encontrada";
+    echo "Pessoa não encontrada";
 }
 ?>
