4e8b95f2567fa3bea4128a834229ea8bbaeddfc6
Julio Batista Silva <julio@juliobs.com>
2016-08-09 16:23:41 -0300

    mensagem de erro

--- interface/conexao.php
+++ interface/conexao.php
@@ -6,7 +6,6 @@ $db_host = "127.0.0.1";
 $con = mysqli_connect($db_host,$db_user,$db_pass,$db_name);
 
 if(!$con){
-    echo "<p>Erro" .mysqli_connect_error() ."</p>";
-    die("Can't connect to database");
+    die("Não foi possível conectar ao banco de dados");
 }
 ?>
